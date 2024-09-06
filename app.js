b3b32fcca78f705f543e176b7274863ee555a793
Nafi' Hasballah Noor <enha0090@gmail.com>
2024-09-06 23:30:21 +0700

    feat(logging):add sentry

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require("./config/lib/instrument");
+var Sentry = require("@sentry/node");
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -5,16 +7,23 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+Sentry.setupExpressErrorHandler(app);
+app.use(function onError(err, req, res, next) {
+    res.statusCode = 500;
+    res.end(res.sentry + "\n");
+});
 
 module.exports = app;
---
+++ config/lib/instrument.js
@@ -0,0 +1,18 @@
+// Import with `import * as Sentry from "@sentry/node"` if you are using ESM
+const Sentry = require("@sentry/node");
+const {
+    nodeProfilingIntegration
+} = require("@sentry/profiling-node");
+require('dotenv').config();
+
+Sentry.init({
+    dsn: process.env.SENTRY_URL,
+    integrations: [
+        nodeProfilingIntegration(),
+    ],
+    // Tracing
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+
+    // Set sampling rate for profiling - this is relative to tracesSampleRate
+    profilesSampleRate: 1.0,
+});
